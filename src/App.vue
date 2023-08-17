23457ba546910ea87d0c9c451be94cbbc5a38be7
Connor <jdzhuzhuoning@163.com>
2023-08-17 19:37:59 +0800

    2023.8.17

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="wrapper">
 <!--    <TableSheet :data-source="data" :default-config="defaultConfig"></TableSheet>-->
-    <TableSheet :data-source="data"></TableSheet>
+<!--    <TableSheet :data-source="data"></TableSheet>-->
 <!--        <ey-table-sheet :data-source="data" :default-config="defaultConfig"></ey-table-sheet>-->
-<!--        <ey-table-sheet :data-source="data"></ey-table-sheet>-->
+        <ey-table-sheet :data-source="data"></ey-table-sheet>
   </div>
 </template>
 <script lang="ts" setup>
 // import TableSheet from './components/TableSheet.vue'
+
 //@ts-ignore
-import TableSheet from '@/ey-table-sheet.common.js'
+// import EyTableSheet from 'ey-table-sheet'
+// import 'ey-table-sheet/dist/ey-table-sheet.css'
 
 // TableSheet.props.defaultConfig.default={
 //   buttons:['保存','刷新'],
--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,22 @@ import store from './store'
 import './assets/main.css'
 
 //@ts-ignore
-// import EyTableSheet from 'dist/ey-table-sheet.common.js'
-// import 'ey-table-sheet/dist/ey-table-sheet.css'
+import EyTableSheet from 'ey-table-sheet'
+import 'ey-table-sheet/dist/ey-table-sheet.css'
 
 // import "@grapecity/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css";
 import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css'
 
-// TableSheet.props.defaultConfig.default={
-//     buttons:['保存','刷新'],
-//     registerCommands:(spread:any)=>{
-//
-//     }
-// }
+EyTableSheet.props.defaultConfig.default={
+    buttons:['保存','刷新'],
+    registerCommands:(spread:any)=>{
+
+    }
+}
 
 let app = createApp(App);
 
-// app.use(EyTableSheet);
+app.use(EyTableSheet);
 // app.component('ey-table-sheet',EyTableSheet);
 
 app.use(store).use(router).mount('#app')
